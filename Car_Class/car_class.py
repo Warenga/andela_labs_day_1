381d0d7b3475c26ebe4dd9c0e307a2f148c000be
Warenga <warengam@gmail.com>
2016-11-11 17:09:39 +0300

    Car Class modified

--- Car_Class/car_class.py
+++ Car_Class/car_class.py
@@ -1,18 +1,37 @@
-class Vehicle(object):
-	name = "General"
-	model = "GM"
 
-	def __init__(self, *args):
-		pass
+class Car(object):
+
+	num_of_wheels = 4
+	speed = 0
+
+	def __init__(self, name='General', model='GM', v_type=''):
+		self.name = name
+		self.model = model
+		self.v_type = v_type
+
+	@property
+	def num_of_doors(self):
+	    if self.name == 'Porsche' or self.name == 'Koenigsegg':
+			self.num_of_doors = 2
+			return self.num_of_door
+		else: 
+			return self.num_of_doors = 4
+
+	def is_saloon(self):
+		self.v_type = 'Saloon'
+
+	def drive(self, drive):
+		if self.v_type == 'trailer':
+			if drive == 7:
+				self.speed = 77
+		else:
+			if drive == 3:
+				self.speed = 1000
 
 
-class Car(Vehicle):
 
-	def __init__(self, *args):
-		super(Vehicle, self).__init__(self, *args)
-		self.name = args[1]
-		self.model = args[2]
-		self.v_type = args[3]
+
+
 
 		
 
