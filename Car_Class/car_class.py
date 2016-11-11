b784298128f37a48c5c922e61607913e5ab381fb
Warenga <warengam@gmail.com>
2016-11-11 17:23:11 +0300

    __init__ changes on num_of_doors and num_of_wheels

--- Car_Class/car_class.py
+++ Car_Class/car_class.py
@@ -1,32 +1,34 @@
 
 class Car(object):
 
+	num_of_doors = 4
 	num_of_wheels = 4
 	speed = 0
+	
 
-	def __init__(self, name='General', model='GM', v_type=''):
+	def __init__(self, name='General', model='GM', v_type='saloon'):
 		self.name = name
 		self.model = model
 		self.v_type = v_type
-
-	@property
-	def num_of_doors(self):
-	    if self.name == 'Porsche' or self.name == 'Koenigsegg':
+		if self.name == 'Porsche' or self.name == 'Koenigsegg':
 			self.num_of_doors = 2
-			return self.num_of_door
-		else: 
-			return self.num_of_doors = 4
+		if self.v_type == 'trailer':
+			self.num_of_wheels = 8
 
 	def is_saloon(self):
-		self.v_type = 'Saloon'
+		self.v_type = 'saloon'
 
 	def drive(self, drive):
+		self.speed = 0
 		if self.v_type == 'trailer':
 			if drive == 7:
 				self.speed = 77
 		else:
 			if drive == 3:
 				self.speed = 1000
+		return self.speed
+
+
 
 
 
